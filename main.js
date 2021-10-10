a94074f884ee4bd215eddb510a8027a6716a837b
James Hansen <41663318+jameszenartist@users.noreply.github.com>
2021-10-10 00:21:19 -0400

    made HTML text, CSS design, and JS changes

--- main.js
+++ main.js
@@ -4,16 +4,13 @@ function showTime() {
   let m = date.getMinutes(); // 0 - 59
   let s = date.getSeconds(); // 0 - 59
   let session = "AM";
-
   if (h == 0) {
     h = 12;
   }
-
   if (h > 12) {
     h = h - 12;
     session = "PM";
   }
-
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
@@ -21,8 +18,6 @@ function showTime() {
   let time = h + ":" + m + ":" + s + " " + session;
   document.querySelector(".time").innerText = time;
   document.querySelector(".time").textContent = time;
-
   setTimeout(showTime, 1000);
 }
-
 showTime();
